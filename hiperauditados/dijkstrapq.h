edbb2b54465c65254aeccec24d5a50701a7b7ba9
Joaquin Romera <joakromera@gmail.com>
2020-12-14 12:59:11 -0300

    clang format dijkstrapq

--- hiperauditados/dijkstrapq.h
+++ hiperauditados/dijkstrapq.h
@@ -1,8 +1,34 @@
-#ifndef DIJKSTRAPQ_H
-#define DIJKSTRAPQ_H
+#pragma once
 
 #include "commonTypes.h"
 
-std::vector<int> dijkstrapq(const adjacencies &adjList, const int &nodes, const int &edges, const int &from);
+// O(v) + O(e * log(e))
+std::vector<int> dijkstrapq(const adjacencies &adjList, const int nodes, const int edges, const int from)
+{
+    std::vector<int> solution(nodes, INF);
+    solution[from] = 0;
 
-#endif
+    priority_Queue queue;
+    queue.emplace(from, 0);
+
+    while (!queue.empty())
+    {
+        const std::tuple<int, int> node = queue.top();
+        queue.pop();
+
+        // Check each node only once
+        if (solution[std::get<0>(node)] < std::get<1>(node)) continue;
+
+        for (const auto &adj : adjList[std::get<0>(node)])
+        {
+            // Relax distances
+            if (solution[std::get<0>(adj)] > solution[std::get<0>(node)] + std::get<1>(adj))
+            {
+                solution[std::get<0>(adj)] = solution[std::get<0>(node)] + std::get<1>(adj);
+                queue.emplace(std::get<0>(adj), solution[std::get<0>(adj)]);
+            }
+        }
+    }
+
+    return solution;
+}
