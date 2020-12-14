99aff6e400b139815b633e2b0af9a866c59fc238
Joaquin Romera <joakromera@gmail.com>
2020-12-14 11:20:35 -0300

    clang format commontypes.h

--- hiperauditados/commonTypes.h
+++ hiperauditados/commonTypes.h
@@ -1,59 +1,60 @@
-#ifndef COMMONTYPES_H
-#define COMMONTYPES_H
+#pragma once
 
-#include <vector>
-#include <tuple>
-#include <queue>
-#include <iostream>
 #include <algorithm>
-#include <set>
 #include <chrono>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <tuple>
+#include <vector>
 
 const int INF = 6000000;
 
 //Incidences List
-typedef std::tuple < int, int, int > edge;
-typedef std::vector < edge > incidences;
+using edge = std::tuple<int, int, int>;
+using incidences = std::vector<edge>;
 
 //Adjacencies List
-typedef std::vector < std::tuple < int, int >> adjacents;
-typedef std::vector < adjacents > adjacencies;
+using adjacents = std::vector<std::tuple<int, int>>;
+using adjacencies = std::vector<adjacents>;
 
-class heavierThan {
-    public:
-        bool operator()(const edge & a,
-            const edge & b) const {
-            return (std::get < 2 > (a) > std::get < 2 > (b));
-        }
+class heavierThan
+{
+public:
+    bool operator()(const edge &a, const edge &b) const
+    {
+        return (std::get<2>(a) > std::get<2>(b));
+    }
 };
 
-class heavier_Than {
-    public:
-        bool operator()(const std::tuple < int, int > & a,
-            const std::tuple < int, int > & b) const {
-            return (std::get < 1 > (a) > std::get < 1 > (b));
-        }
+class heavier_Than
+{
+public:
+    bool operator()(const std::tuple<int, int> &a, const std::tuple<int, int> &b) const
+    {
+        return (std::get<1>(a) > std::get<1>(b));
+    }
 };
 
-class heavier_Than_A {
-    public:
-        bool operator()(const std::tuple < int, int, int > & a,
-            const std::tuple < int, int, int > & b) const {
-            return (std::get < 2 > (a) > std::get < 2 > (b));
-        }
+class heavier_Than_A
+{
+public:
+    bool operator()(const std::tuple<int, int, int> &a, const std::tuple<int, int, int> &b) const
+    {
+        return (std::get<2>(a) > std::get<2>(b));
+    }
 };
 
-class smallerThan {
-    public:
-        bool operator()(const edge & a,
-            const edge & b) const {
-            return (std::get < 2 > (a) < std::get < 2 > (b));
-        }
+class smallerThan
+{
+public:
+    bool operator()(const edge &a, const edge &b) const
+    {
+        return (std::get<2>(a) < std::get<2>(b));
+    }
 };
 
 //Priority Queue
-typedef std::priority_queue < edge, std::vector < edge > , heavierThan > priorityQueue;
-typedef std::priority_queue < std::tuple < int, int > , std::vector < std::tuple < int, int > > , heavier_Than > priority_Queue;
-typedef std::priority_queue < std::tuple < int, int, int > , std::vector < std::tuple < int, int, int > > , heavier_Than_A > priority_Queue_A;
-
-#endif
+using priorityQueue    = std::priority_queue<edge, std::vector<edge>, heavierThan>;
+using priority_Queue   = std::priority_queue<std::tuple<int, int>, std::vector<std::tuple<int, int>>, heavier_Than>;
+using priority_Queue_A = std::priority_queue<std::tuple<int, int, int>, std::vector<std::tuple<int, int, int>>, heavier_Than_A>;
