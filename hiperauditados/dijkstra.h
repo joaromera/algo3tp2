8a904ebaabd6688966deacc62c250abad19397cb
Joaquin Romera <joakromera@gmail.com>
2020-12-14 12:02:53 -0300

    clang format dijkstra

--- hiperauditados/dijkstra.h
+++ hiperauditados/dijkstra.h
@@ -1,8 +1,51 @@
-#ifndef DIJKSTRA_H
-#define DIJKSTRA_H
+#pragma once
 
 #include "commonTypes.h"
 
-std::vector<int> dijkstra(const adjacencies &adjList, const int &nodes, const int &edges, const int &from);
+// O(v^2 + e)
+std::vector<int> dijkstra(const adjacencies &adjList, const int nodes, const int edges, const int from)
+{
+    std::vector<bool> visited(nodes, false);
+    visited[from] = true;
 
-#endif
+    // Actualizo las distancias desde el nodo de partida hasta sus vecinos
+    std::vector<int> solution(nodes, INF);
+    for (const auto &adj : adjList[from])
+    {
+        solution[std::get<0>(adj)] = std::get<1>(adj);
+    }
+    solution[from] = 0;
+
+    std::vector<int> solution_tmp = solution;
+
+    // Mientras no haya chequeado todos los nodos
+    for (int checked_nodes = 1; checked_nodes != nodes; )
+    {
+        std::tuple<int, int> minNode = { from, INF };
+
+        // Busco el nodo con mínima distancia que aun no haya sido visitado
+        for (size_t i = 0; i < nodes; ++i)
+        {
+            const int minNodeW = std::get<1>(minNode);
+            if (!visited[i] && minNodeW >= solution[i])
+            {
+                minNode = { i, solution[i] };
+            }
+        }
+
+        const int minNodeIndex = std::get<0>(minNode);
+        visited[minNodeIndex] = true;
+        ++checked_nodes;
+
+        for (const auto &adj : adjList[minNodeIndex])
+        {
+            const int adjIndex = std::get<0>(adj);
+            const int adjW     = std::get<1>(adj);
+            if (solution[adjIndex] > solution[minNodeIndex] + adjW)
+            {
+                solution[adjIndex] = solution[minNodeIndex] + adjW;
+            }
+        }
+    }
+    return solution;
+}
