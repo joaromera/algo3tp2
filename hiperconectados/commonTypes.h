d63feae854422181eb5fdb10ac8b4ccb296557ac
Joaquin Romera <joakromera@gmail.com>
2020-12-14 22:10:44 -0300

    common types update

--- hiperconectados/commonTypes.h
+++ hiperconectados/commonTypes.h
@@ -1,5 +1,4 @@
-#ifndef COMMONTYPES_H
-#define COMMONTYPES_H
+#pragma once
 
 #include <algorithm>
 #include <chrono>
@@ -10,7 +9,7 @@
 #include <tuple>
 #include <vector>
 
-const int INF = std::numeric_limits<int>::max();
+constexpr int INF = std::numeric_limits<int>::max();
 
 //Incidences List
 typedef std::tuple<int, int, int> edge;
@@ -20,21 +19,19 @@ typedef std::vector<edge> incidences;
 typedef std::vector<std::tuple<int, int>> adjacents;
 typedef std::vector<adjacents> adjacencies;
 
-class heavierThan
+class EdgeHeavierThan
 {
 public:
-    bool operator()(const edge &a,
-        const edge &b) const
+    bool operator()(const edge &a, const edge &b) const
     {
         return (std::get<2>(a) > std::get<2>(b));
     }
 };
 
-class heavier_Than
+class TupleHeavierThan
 {
 public:
-    bool operator()(const std::tuple<int, int> &a,
-        const std::tuple<int, int> &b) const
+    bool operator()(const std::tuple<int, int> &a, const std::tuple<int, int> &b) const
     {
         return (std::get<1>(a) > std::get<1>(b));
     }
@@ -43,26 +40,13 @@ public:
 class heavier_Than_A
 {
 public:
-    bool operator()(const std::tuple<int, int, int> &a,
-        const std::tuple<int, int, int> &b) const
+    bool operator()(const std::tuple<int, int, int> &a, const std::tuple<int, int, int> &b) const
     {
         return (std::get<2>(a) > std::get<2>(b));
     }
 };
 
-class smallerThan
-{
-public:
-    bool operator()(const edge &a,
-        const edge &b) const
-    {
-        return (std::get<2>(a) < std::get<2>(b));
-    }
-};
-
 //Priority Queue
-typedef std::priority_queue<edge, std::vector<edge>, heavierThan> priorityQueue;
-typedef std::priority_queue<std::tuple<int, int>, std::vector<std::tuple<int, int>>, heavier_Than> priority_Queue;
-typedef std::priority_queue<std::tuple<int, int, int>, std::vector<std::tuple<int, int, int>>, heavier_Than_A> priority_Queue_A;
-
-#endif
+using priorityQueue = std::priority_queue<edge, std::vector<edge>, EdgeHeavierThan>;
+using priority_Queue = std::priority_queue<std::tuple<int, int>, std::vector<std::tuple<int, int>>, TupleHeavierThan>;
+using priority_Queue_A = std::priority_queue<std::tuple<int, int, int>, std::vector<std::tuple<int, int, int>>, heavier_Than_A>;
